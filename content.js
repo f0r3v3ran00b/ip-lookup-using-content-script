3c5212b15dbe08fea626318fc3de01baa974eb80
Sanjay Srinivas <srinivassanjay@gmail.com>
2019-01-09 16:21:49 +1100

    Initial commit

---
+++ content.js
@@ -0,0 +1,14 @@
+const lookupIP = () => {
+    fetch(`https://extreme-ip-lookup.com/json/`)
+        .then((response) => {
+            return response.json()
+        }).
+        then((jsonData) => {
+            console.log(`Your IP is ${jsonData.query} / ${jsonData.city} / ${jsonData.country}`)
+            $(`<div style="background-color: darkorange; color: black">Your IP is ${jsonData.query} ${jsonData.city}  ${jsonData.country}</div>`).prependTo('body');
+            return jsonData
+        }).catch((error) => {
+            alert(error)
+        })
+}
+lookupIP()
